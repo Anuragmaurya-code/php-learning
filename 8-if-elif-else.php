b87ee8e4bd934e487f887cab151effbc4418ee03
Anurag Maurya <mauryaanurag375@gmail.com>
2023-10-30 22:56:37 +0530

    if else

---
+++ 8-if-elif-else.php
@@ -0,0 +1,22 @@
+<?php
+    // $age=1001;
+    // if($age>=100){
+    //     echo"too old to enter";
+    // }
+    // elseif($age>=18){
+    //     echo"You may enter this side";
+    // }elseif($age<=0){
+    //     echo"Not a valid age";
+    // }
+    // else{
+    //     $req=18-$age;
+    //     echo"come after {$req} years to enter ";
+    // } 
+    $adult=false;
+    if($adult){
+        echo"You are adult";
+    }
+    else{
+        echo"kid go home";
+    }
+?>
