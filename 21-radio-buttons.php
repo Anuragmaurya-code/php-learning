0a6b12c250c4cbed9a5e1c2d02618d7fa8c60588
Anurag Maurya <mauryaanurag375@gmail.com>
2023-10-31 15:46:36 +0530

    radio buttons

---
+++ 21-radio-buttons.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="index.php" method="post">
+        <input type="radio" name="credit_card" value="visa">Visa<br>
+        <input type="radio" name="credit_card" value="Master card">Master card<br>
+        <input type="radio" name="credit_card" value="American Express">Americal Express<br>
+        <input type="submit" name="confirm" value="confirm">
+    </form>
+
+</body>
+
+</html>
+<?php
+// if (isset($_POST["confirm"])) {
+//     if (isset($_POST["credit_card"])) {
+//         $credit_card = $_POST["credit_card"];
+//         echo "you selected {$credit_card}";
+//     }
+//     else{
+//         echo"Please, select an option";
+//     }
+// }
+if(isset($_POST['confirm'])){
+    $credit_card=null;
+if(isset($_POST['credit_card'])){
+    $credit_card=$_POST['credit_card'];
+}
+switch ($credit_card){
+    case 'visa':
+        echo "You selected Vise card";
+        break;
+    case 'Master card':
+        echo "You selected Master card";
+        break;
+    case 'American Express':
+        echo "You selected American Express card";
+        break;
+    default:
+        echo "select a carrd";
+}
+
+}
+
+?>
